3d0ff29999ae58ea50900c4aaae1d4715ab034bd
Shawn <sh117739@ucf.edu>
2025-02-03 16:20:23 -0500

    boss code start

--- CupheadLikeGame/Assets/Scripts/boss code.cs
+++ CupheadLikeGame/Assets/Scripts/boss code.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public int bossHealth = 20;
+    public int currentHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentHealth = bossHealth;
     }
 
     // Update is called once per frame

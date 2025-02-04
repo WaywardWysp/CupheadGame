539190cb2b954ebf947835c5eb7d8a0abf2a5c8f
Shawn <sh117739@ucf.edu>
2025-02-04 00:29:54 -0500

    boss attack timer

--- CupheadLikeGame/Assets/Scripts/boss code.cs
+++ CupheadLikeGame/Assets/Scripts/boss code.cs
@@ -4,7 +4,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public int bossHealth = 20;
     public int currentHealth;
+    public GameObject target;
+    public GameObject trigger1;
+    public GameObject trigger2;
+    public float attackCooldown = 3f;
 
+    public bool canAttack = true;
+
+    public bool timerIsRunning = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +21,49 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        target = GameObject.Find("player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player") && canAttack)
+        {
+            //this is where your attacks will go
+
+            if (collision.gameObject == trigger1)
+            {
+                timerIsRunning = true;
+                if (attackCooldown > 0)
+                {
+                    attackCooldown -= Time.deltaTime;
+
+                }
+                else
+                {
+                    attackCooldown = 0;
+                    timerIsRunning = false;
+                    canAttack = true;
+
+                }
+            }
+
+            if (collision.gameObject == trigger2)
+            {
+                timerIsRunning = true;
+                if (attackCooldown > 0)
+                {
+                    attackCooldown -= Time.deltaTime;
+
+                }
+                else
+                {
+                    attackCooldown = 0;
+                    timerIsRunning = false;
+                    canAttack = true;
+                }
+            }
+
+
+        }
     }
 }
